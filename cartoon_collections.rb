efc4844a756d4c4208e3adaa57088b1af18fc8a0
McKayla Lankau <lankaukk@yahoo.com>
2020-08-11 01:31:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do |element|
-      if element == cheese_types 
+      if element.any? == cheese_types 
         return element
       else
         nil 
